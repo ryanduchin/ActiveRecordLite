aaef7cabc861412189a1f660a3ec95fe60da5753
App Academy Student <student@appacademy.io>
2015-05-08 18:26:29 -0700

    fixed phase III

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -57,60 +57,29 @@ module Associatable
     define_method(name) do
       foreign_key = options.send(:foreign_key) #this returns :owner_id but i need the value
       # p "foreign_key #{foreign_key}"
-      referenced_class = self.class
-      # p "referenced_class #{referenced_class}"
       model_class = options.send(:model_class) #human
-      # p "model_class #{model_class}"
-      tableA = referenced_class.table_name #cats
-      # p "tableA #{tableA}"
-      tableB = model_class.table_name #humans...
-      # p "tableB #{tableB}"
-      # p "self #{self}"
-      target_id = self.send(foreign_key) #cat.owner_id
-      # p "cat_id #{cat_id}"
-
-      results = DBConnection.execute(<<-SQL, target_id)
-        SELECT
-          #{tableB}.*
-        FROM
-          #{tableB}
-        JOIN
-          #{tableA} on #{tableA}.#{foreign_key} = #{tableB}.id
-        WHERE
-          #{tableB}.id = ?
-      SQL
-      model_class.parse_all(results).first
+      # # p "model_class #{model_class}"
+      model_class.find(self.send(foreign_key))
     end
   end
 
   def has_many(name, options = {})
     options = HasManyOptions.new(name, self, options)
-    define_method(name) do
-      foreign_key = options.send(:foreign_key) #this returns :owner_id but i need the value
-      p "foreign_key #{foreign_key}"
-      referenced_class = self.class
-      p "referenced_class #{referenced_class}"
-      model_class = options.send(:model_class) #human
-      p "model_class #{model_class}"
-      tableA = referenced_class.table_name #cats
-      p "tableA #{tableA}"
-      tableB = model_class.table_name #humans...
-      p "tableB #{tableB}"
-      p "self #{self}"
-      target_id = self.send(foreign_key) #cat.owner_id
 
-      results = DBConnection.execute(<<-SQL, target_id)
-        SELECT
-          #{tableB}.*
-        FROM
-          #{tableB}
-        JOIN
-          #{tableA} on #{tableA}.#{foreign_key} = #{tableB}.id
-        WHERE
-          #{tableB}.id = ?
-      SQL
-      model_class.parse_all(results).first
+    define_method(name) do
+      foreign_key = options.send(:foreign_key) #owner_id
+      # p "foreign_key #{foreign_key}"
+      model_class = options.send(:model_class) #Cat
+      # p "model_class #{model_class}"
+      result = [] #cats
+      model_class.all.each do |ref|
+        result << ref if ref.send(foreign_key) == self.id
+      end
+      result
     end
+  end
+
+  def has_one_through
   end
 
   def assoc_options
@@ -121,3 +90,49 @@ end
 class SQLObject
   extend Associatable
 end
+
+# belongs_to method text that I did that was totally unnecessary!!!
+# referenced_class = self.class #Human
+# # p "referenced_class #{referenced_class}"
+# referenced_class = self.class
+# # p "referenced_class #{referenced_class}"
+# tableA = referenced_class.table_name #cats
+# # p "tableA #{tableA}"
+# tableB = model_class.table_name #humans...
+# # p "tableB #{tableB}"
+# # p "self #{self}" #Cat object
+# target_id = self.send(foreign_key) #cat.owner_id
+# # p "cat_id #{cat_id}"
+#
+# results = DBConnection.execute(<<-SQL, target_id)
+#   SELECT
+#     #{tableB}.*
+#   FROM
+#     #{tableB}
+#   JOIN
+#     #{tableA} on #{tableA}.#{foreign_key} = #{tableB}.id
+#   WHERE
+#     #{tableB}.id = ?
+# SQL
+# model_class.parse_all(results).first
+
+
+
+
+# has_many text that I did that was totally unnecessary!
+# tableA = referenced_class.table_name #humans
+# # p "tableA #{tableA}"
+# tableB = model_class.table_name #cats
+# # p "tableB #{tableB}"
+# # p "self.id #{self.id}" # Human object's id
+# results = DBConnection.execute(<<-SQL, self.id)
+#   SELECT
+#     #{tableB}.*
+#   FROM
+#     #{tableB}
+#   JOIN
+#     #{tableA} on #{tableB}.#{foreign_key} = #{tableA}.id
+#   WHERE
+#   #{tableB}.#{foreign_key} = ?
+# SQL
+# model_class.parse_all(results)
